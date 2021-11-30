de6ec59797d00ba1af96656fe84aff3861025461
ILYA655 <75269869+ILYA655@users.noreply.github.com>
2021-11-30 12:42:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,263 +1,310 @@
-#include <stdio.h>
-#include <iostream>
-#include <locale.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <queue>
-
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<conio.h>
+#include<time.h>
+#include<stack>
+#include<queue>
 using namespace std;
 
-void BFSD(int** G, int n, int* dist, int s)
+int** neor_vz(int n)
 {
-	queue<int> q;
-	q.push(s);
-    dist[s] = 0;
-	while (!q.empty())
+	int s = 1;
+	int** Arr;
+
+	Arr = (int**)malloc(n * sizeof(int*));
+	srand(time(NULL));
+	for (int i = 0; i <n; i++)
 	{
-		s = q.front();
-		q.pop();
-		cout << s << " ";
-		for (int j = 0; j < n; j++)
+		Arr[i] = (int*)malloc(n * sizeof(int));
+		Arr[i][i] = 0;
+
+		for (int j = s; j <n; j++)
 		{
-			if (G[s][j] > 0 && dist[j] > dist[s] + G[s][j])
+			if (rand() % 100 > 70)
 			{
-				dist[j] = dist[s] + G[s][j];
-				q.push(j);
+				Arr[i][j] = 0;
+			}
+			else
+			{
+				Arr[i][j] = rand() % n;
+			}
+		}
+		s++;
+	}
+	s = 1;
+	for (int i = 0; i <n; i++)
+	{
+		for (int j = s; j <n; j++)
+			Arr[j][i] = Arr[i][j];
+		s++;
+	}
+
+	for (int i = 0; i <n; i++)
+		printf("V%d ", i + 1);
+
+	for (int i = 0; i <n; i++)
+	{
+		printf("\n");
+		for (int j = 0; j <n; j++)
+		{
+			printf("%2d ", Arr[i][j]);
+		}
+	}
+	return Arr;
+}
+
+int** orent_vz(int n)
+{
+	int** Arr2 = (int**)malloc(n * sizeof(int*));
+	srand(time(NULL));
+	for (int i = 0; i <n; i++)
+	{
+		Arr2[i] = (int*)malloc(n * sizeof(int));
+
+		for (int j = 0; j <n; j++)
+		{
+			if (rand() % 100 > 70)
+			{
+				Arr2[i][j] = 0;
+			}
+			else
+			{
+				Arr2[i][j] = rand() % n;
+			}
+		}
+	}
+	for (int i = 0; i <n; i++)
+	{
+		Arr2[i][i] = 0;
+		for (int j = 0; j <n; j++)
+		{
+			if (Arr2[i][j] > 0)
+				Arr2[j][i] = 0;
+		}
+	}
+	printf("\n");
+	for (int i = 0; i <n; i++)
+		printf("V%d ", i + 1);
+	for (int i = 0; i <n; i++)
+	{
+		printf("\n");
+		for (int j = 0; j <n; j++)
+			printf("%2d ", Arr2[i][j]);
+	}
+	return Arr2;
+}
+
+int** neor_nevz(int n)
+{
+	int s = 1;
+	int** Arr;
+
+	Arr = (int**)malloc(n * sizeof(int*));
+	srand(time(NULL));
+	for (int i = 0; i <n; i++)
+	{
+		Arr[i] = (int*)malloc(n * sizeof(int));
+		Arr[i][i] = 0;
+
+		for (int j = s; j <n; j++)
+		{
+			if (rand() % 100 > 70)
+			{
+				Arr[i][j] = 0;
+			}
+			else {
+				Arr[i][j] = 1;
+			}
+		}
+		s++;
+	}
+	s = 1;
+	for (int i = 0; i <n; i++)
+	{
+		for (int j = s; j <n; j++)
+			Arr[j][i] = Arr[i][j];
+		s++;
+	}
+
+	for (int i = 0; i <n; i++)
+		printf("V%d ", i + 1);
+
+	for (int i = 0; i <n; i++)
+	{
+		printf("\n");
+		for (int j = 0; j <n; j++)
+		{
+			printf("%2d ", Arr[i][j]);
+		}
+	}
+	return Arr;
+}
+
+int** orent_nevz(int n)
+{
+	int** Arr2 = (int**)malloc(n * sizeof(int*));
+	srand(time(NULL));
+	for (int i = 0; i <n; i++)
+	{
+		Arr2[i] = (int*)malloc(n * sizeof(int));
+
+		for (int j = 0; j <n; j++)
+		{
+			if (rand() % 100 > 70)
+			{
+				Arr2[i][j] = 0;
+			}
+			else
+			{
+				Arr2[i][j] = 1;
+			}
+		}
+	}
+	for (int i = 0; i <n; i++)
+	{
+		Arr2[i][i] = 0;
+		for (int j = 0; j <n; j++)
+		{
+			if (Arr2[i][j] > 0)
+				Arr2[j][i] = 0;
+		}
+	}
+	printf("\n");
+	for (int i = 0; i <n; i++)
+		printf("V%d ", i + 1);
+	for (int i = 0; i <n; i++)
+	{
+		printf("\n");
+		for (int j = 0; j <n; j++)
+			printf("%2d ", Arr2[i][j]);
+	}
+	return Arr2;
+}
+
+void BFSD(int x, int* dist, int** Arr, int n, int** distX)
+{
+	int j = 0;
+	queue<int> que;
+	que.push(x);
+	dist[x] = 0;
+	while (!que.empty())
+	{
+		x = que.front();
+		que.pop();
+		for (int i = 0; i <n; i++)
+		{
+			if (Arr[x][i] > 0 &&dist[i] >dist[x] + Arr[x][i])
+			{
+				que.push(i);
+				dist[i] = dist[x] + Arr[x][i];
+				j = 0;
+				while (distX[j][x] != 0)
+				{
+					distX[j][i] = distX[j][x];
+					j++;
+				}
+				distX[j][i] = i + 1;
 			}
 		}
 	}
 }
 
-int** create(int n) 
+int main()
 {
-	int** G, k = 1;
-	G = (int**)malloc(n * sizeof(int*));
-	srand(time(NULL));
-	for (int i = 0; i < n; i++)
+	setlocale(LC_ALL, "Ru");
+	int n, ** n_v, x, ** o_v, ** n_n, ** o_n;
+	char* d;
+	printf("Введите размер матрицы: ");
+	scanf("%d", &n);
+	printf("Введите, с какой вершины хотите начать: ");
+	scanf("%d", &x);
+	while (x < 1 || x > n)
 	{
-		G[i] = (int*)malloc(n * sizeof(int));
-		G[i][i] = 0;
-		for (int j = k; j < n; j++)
-		{
-			G[i][j] = (rand() % 10);
-		}
-		k++;
+		printf("С этой вершины нельзя начинать! Выберите другю вершину: ");
+		scanf("%d", &x);
 	}
-	k = 1;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = k; j < n; j++)
-		{
-			G[j][i] = G[i][j];
-		}
-		k++;
-	}
-
-	for (int i = 0; i < n; i++)
-		cout << "V" << i << " ";
-
-	for (int i = 0; i < n; i++)
-	{
-		cout << endl;
-		for (int j = 0; j < n; j++)
-		{
-			cout << G[i][j] << "  ";
-		}
-	}
-	return G;
-}
-
-int** create_o(int n) 
-{
-	int** G, k = 1;
-	G = (int**)malloc(n * sizeof(int*));
-	srand(time(NULL));
-	for (int i = 0; i < n; i++)
-	{
-		G[i] = (int*)malloc(n * sizeof(int));
-		G[i][i] = 0;
-		for (int j = k; j < n; j++)
-		{
-			G[i][j] = (rand() % 10);
-		}
-		k++;
-	}
-	k = 1;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = k; j < n; j++)
-		{
-			G[j][i] = (rand() % 10);
-		}
-		k++;
-	}
-
-	for (int i = 0; i < n; i++)
-		cout << "V" << i << " ";
-
-	for (int i = 0; i < n; i++)
-	{
-		cout << endl;
-		for (int j = 0; j < n; j++)
-		{
-			cout << G[i][j] << "  ";
-		}
-	}
-	return G;
-}
-
-int** create_on(int n) 
-{
-	int** G, k = 1;
-	G = (int**)malloc(n * sizeof(int*));
-	srand(time(NULL));
-	for (int i = 0; i < n; i++)
-	{
-		G[i] = (int*)malloc(n * sizeof(int));
-		G[i][i] = 0;
-		for (int j = k; j < n; j++)
-		{
-			G[i][j] = (rand() % 2);
-		}
-		k++;
-	}
-	k = 1;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = k; j < n; j++)
-		{
-			G[j][i] = (rand() % 2);
-		}
-		k++;
-	}
-
-	for (int i = 0; i < n; i++)
-		cout << "V" << i << " ";
-
-	for (int i = 0; i < n; i++)
-	{
-		cout << endl;
-		for (int j = 0; j < n; j++)
-		{
-			cout << G[i][j] << "  ";
-		}
-	}
-	return G;
-}
-
-int** create_n(int n)
-{
-	int** G, k = 1;
-	G = (int**)malloc(n * sizeof(int*));
-	srand(time(NULL));
-	for (int i = 0; i < n; i++)
-	{
-		G[i] = (int*)malloc(n * sizeof(int));
-		G[i][i] = 0;
-		for (int j = k; j < n; j++)
-		{
-			G[i][j] = (rand() % 2);
-		}
-		k++;
-	}
-	k = 1;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = k; j < n; j++)
-		{
-			G[j][i] = G[i][j];
-		}
-		k++;
-	}
-
-	for (int i = 0; i < n; i++)
-		cout << "V" << i << " ";
-
-	for (int i = 0; i < n; i++)
-	{
-		cout << endl;
-		for (int j = 0; j < n; j++)
-		{
-			cout << G[i][j] << "  ";
-		}
-	}
-	return G;
-}
-
-int main(int argc, char *argv[])
-{
-	int n, s, ** t, ** t1, ** t2, ** t3;
-	cout << "Enter array size: ";
-	cin >> n;
+	x--;
 	int* dist = (int*)malloc(n * sizeof(int));
-	
+	int** distX = (int**)malloc(sizeof(int*));
 	for (int i = 0; i < n; i++)
-		dist[i] = INT16_MAX;
-	if(argc >= 1){
-		for (int i = 0; i < argc; ++i){
-			if ((strcmp(argv[i], "0") == 0)){ //неориентированный взвешенный
-				cout << "неориентированный взвешенный";
-				cout << endl;
-				t = create(n);
-				cout << endl;
-				cout << "Enter vertex: ";
-				cin >> s;
-				cout << "BFSD result: ";
-				BFSD(t, n, dist, s);
-				cout << endl;
-			}
-			if ((strcmp(argv[i], "1") == 0)){ //ориентированный взвешенный
-				cout << "ориентированный взвешенный";
-				cout << endl;
-				t1 = create_o(n);
-				cout << endl;
-				cout << "Enter vertex: ";
-				cin >> s;
-				cout << "BFSD result: ";
-				BFSD(t1, n, dist, s);
-				cout << endl;
-			}
-			if ((strcmp(argv[i], "2") == 0)){ //ориентированный невзвешенный
-				cout << "ориентированный невзвешенный";
-				cout << endl;
-				t2 = create_on(n);
-				cout << endl;
-				cout << "Enter vertex: ";
-				cin >> s;
-				cout << "BFSD result: ";
-				BFSD(t2, n, dist, s);
-				cout << endl;
-			}
-			if ((strcmp(argv[i], "3") == 0)){ //неориентированный невзвешенный
-				cout << "неориентированный невзвешенный";
-				cout << endl;
-				t3 = create_n(n);
-				cout << endl;
-				cout << "Enter vertex: ";
-				cin >> s;
-				cout << "BFSD result: ";
-				BFSD(t3, n, dist, s);
-				cout << endl;
-			}
-		}
+	{
+		distX[i] = (int*)malloc(n * sizeof(int));
+		for (int j = 0; j < n; j++)
+			distX[i][j] = 0;
 	}
 	for (int i = 0; i < n; i++)
-		cout << dist[i] << " ";
-	cout << endl;
-	for (int i = 0; i < n; i++)
-		free(t3[i]);
-	free(t3);
-	for (int i = 0; i < n; i++)
-		free(t2[i]);
-	free(t2);
-	for (int i = 0; i < n; i++)
-		free(t1[i]);
-	free(t1);
-	for (int i = 0; i < n; i++)
-		free(t[i]);
-	free(t);
-	free(dist);
+	{
+		distX[0][i] = 1;
+	}
+
+		printf("\n");
+		n_v = neor_vz(n);
+		for (int i = 0; i < n; i++)
+			dist[i] = n;
+		BFSD(x, dist, n_v, n, distX);
+		for (int i = 0; i < n; i++)
+		{
+			if (dist[i] != n)
+				printf("\nРасстояниедо %d - ойвершины: %d ", i + 1, dist[i]);
+			else
+				printf("\nДо вершины %d - нет расстояния", i + 1);
+		}
+		printf("\n");
+
+		for (int i = 0; i < n; i++)
+		{
+			printf("\n");
+
+			for (int j = 0; j < n; j++)
+				printf("%d ", distX[i][j]);
+		}
+		for (int j = 0; j < n; j++)
+		//printf("\n");
+		//o_n = orent_nevz(n);
+		//for (int i = 0; i < n; i++)
+		//	dist[i] = INT_MAX;
+		//BFSD(x, dist, o_n, n, distX);
+		//for (int i = 0; i < n; i++)
+		//{
+		//	if (dist[i] != INT_MAX)
+		//		printf("\nРасстояние до %d - ой вершины: %d ", i + 1, dist[i]);
+		//	else
+		//		printf("\nДо вершины %d - нет расстояния", i + 1);
+		//}
+		//printf("\n");
+
+
+		//printf("\n");
+		//n_n = neor_nevz(n);
+		//for (int i = 0; i < n; i++)
+		//	dist[i] = INT_MAX;
+		//BFSD(x, dist, n_n, n, distX);
+		//for (int i = 0; i < n; i++)
+		//{
+		//	if (dist[i] != INT_MAX)
+		//		printf("\nРасстояние до %d - ой вершины: %d ", i + 1, dist[i]);
+		//	else
+		//		printf("\nДо вершины %d - нет расстояния", i + 1);
+		//}
+		//printf("\n");
+
+
+		//printf("\n");
+		//o_v = orent_vz(n);
+		//for (int i = 0; i < n; i++)
+		//	dist[i] = INT_MAX;
+		//BFSD(x, dist, o_v, n, distX);
+		//for (int i = 0; i < n; i++)
+		//{
+		//	if (dist[i] != INT_MAX)
+		//		printf("\nРасстояние до %d - ой вершины: %d ", i + 1, dist[i]);
+		//	else
+		//		printf("\nДо вершины %d - нет расстояния", i + 1);
+		//}
+		//printf("\n");
+
+
 	system("pause");
-	return 0;
 }
